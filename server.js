21cd276b3842a08951c6767bbc7bd01e011367c1
Devank Gupta <devankgupta284@gmail.com>
2025-09-04 01:31:45 +0530

    done

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app= express();
 const db= require('./db');
 
 const bodyParser= require('body-parser');
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //req.body
 
 const Person = require('./models/Person');
 
@@ -13,7 +13,50 @@ app.get('/',function(req, res){
     res.send('Welcome to my hotel... How i can help you? , we have list of menus')
 })
 
+app.post('/person', async (req,res)=>{
 
+
+    try{
+
+        const data = req.body //Assuming the request body contains the person data
+
+    //Cretae a new Person document using the mongoose model
+
+     const newPerson = new Person(data);
+
+     //save the newPerson to the database
+
+    const response = await newPerson.save();
+    console.log('data saved');
+    res.status(200).json(response);
+
+
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+})
 
 
 app.listen(3000,()=>{
