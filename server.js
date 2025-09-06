cdc43630f32041b21efcdd01d472a3a5cfd2b06a
Devank Gupta <devankgupta284@gmail.com>
2025-09-06 19:12:13 +0530

    Authenticaton done

--- server.js
+++ server.js
@@ -4,17 +4,36 @@ const app= express();
 
 const db= require('./db');
 
+const passport=require('./auth');
+
+
+
+
+
+
+
 const bodyParser= require('body-parser');
 app.use(bodyParser.json()); //req.body
 
+//Middleware Function
+const logRequest = (req,res,next)=>{
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); //Move on the next phase
+}
+
+app.use(logRequest);
 
 
+
+app.use(passport.initialize());
+
+const localAuthMiddleware = passport.authenticate('local',{session:false});
 
 app.get('/',function(req, res){
     res.send('Welcome to our Hotel');
 })
 
-
+  
 
 
 
@@ -22,7 +41,7 @@ app.get('/',function(req, res){
 // Import the router files and use the routers
 
 const personRoutes= require('./routes/personRoutes');
-app.use('/person',personRoutes);
+app.use('/person',localAuthMiddleware,personRoutes);
 
 
 const menuItemRoutes = require('./routes/menuItemRoutes');
